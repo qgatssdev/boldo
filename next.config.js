e26c659ee9ae895b06af0dfa29fc7ee8779eacde
qgatssdev <chivoomodu@gmail.com>
2023-08-22 22:17:53 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['page.tsx', 'page.jsx'],
+  output: 'standalone',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
